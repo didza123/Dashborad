c133675b74874c703b6b1798c60466aca59af570
unknown <ologydidza@gmail.com>
2024-03-26 11:13:44 +0200

    Added updated data

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Admin, Resource} from 'react-admin';
+import jsonServerProvider from 'ra-data-simple-rest';
+import {PostList} from '../src/components/PostList';
+
+const App = () => 
+(<Admin dataProvider={jsonServerProvider(fetch('http://localhost:3000/'))}>
+  <Resource name='posts' list={PostList} />
+</Admin>
+);
+
+export default App;
